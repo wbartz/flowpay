33c54183292114aa268229442e74943df4141177
William Bartz <williamf.brtz@gmail.com>
2025-11-26 18:54:40 -0300

    chore: fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import js from '@eslint/js';
+import globals from 'globals';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
@@ -7,12 +8,11 @@ import jest from 'eslint-plugin-jest';
 export default [
   js.configs.recommended,
   {
-    env: {
-      node: true,
-      commonjs: true
-    },
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
+      globals: {
+        ...globals.node
+      },
       parser: tsparser,
       ecmaVersion: 'latest',
       sourceType: 'module',
